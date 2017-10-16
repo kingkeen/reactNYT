e755171abe468330f58cc81be1f22bac3f55aaf5
Joaquin Ignacio <Keen@Revolt.local>
2017-10-15 23:03:31 -0700

    first component for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
+        <br />
         <div className="App-header">
           <h2>React to NYT Search!</h2>
         </div>
